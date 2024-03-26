0fb7441943700e8ab1d8f3388f49ed4cf64fa1e7
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-03-26 20:05:38 +0000

    datatype

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  // we are using nodejs,not browsre
+
+let name="saquib"
+let age="24"
+let isLoggedIn= false
+
+//java script Data Type
+
+//number => 2 to power 53
+//Bigint
+//string =>""
+//boolean => true/false
+//null => stand alone value
+//undefined =>
+//symbole =>unique
+
+//above are primitive data type
+
+//object
+
+console.log(typeof undefined);
+
+console.log(typeof null);
