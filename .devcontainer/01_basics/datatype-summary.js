9bfa77690be1abe21438cec6b955e1798b98ac26
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-03-29 18:53:55 +0000

    datatype-summary

---
+++ .devcontainer/01_basics/datatype-summary.js
@@ -0,0 +1,41 @@
+//data type
+
+//primitive
+
+//7 types: string, Number, Boolean, null , undifined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//sumbole give the like unique value in a program.if we have same value of diffrent variable then the symbole says that not the both value is same.
+
+console.log(id===anotherId);
+
+const bigNumber = 123456789n //when we use the 'n' in the last of number then it automaticaly convert into BigInt value.
+
+//Refrence (Non Primtive)
+
+//Array, Object, Function
+
+const heros = ["shaktiman", "ironman", "batman"];
+
+let myObj = {
+
+    name:"saquib"
+    age:24
+}
+
+
+//Function(){} (syntax of Function)
+const myFunction = function(){
+
+    console.log("Hello world");
+}
