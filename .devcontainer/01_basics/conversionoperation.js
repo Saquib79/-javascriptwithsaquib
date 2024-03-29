d87ce2d6663f939ac035702aa70a2adec3fb0387
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-03-29 10:49:30 +0000

    operation

--- .devcontainer/01_basics/conversionoperation.js
+++ .devcontainer/01_basics/conversionoperation.js
@@ -1,10 +1,10 @@
 //let score= "33abc"
 //let score= null
-let score=undefined
+//let score=undefined
 //let score=Boolean
 //let score="saquib"
 
-console.log(typeof score);
+//console.log(typeof score);
 //console.log(typeof(score));
 
 //let valueInNumber= Number(score)
@@ -15,16 +15,42 @@ console.log(typeof score);
 // "33abc" =>NaN
 // true => 1; false =>0
 
-let isLoggedIn= 1
+//let isLoggedIn= 1
 
-let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+// ***********Operation************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Saquib"
+let str2 =  " sadik"
+let str3 = str1 + str2
+
+console.log(str3);
+
+let gameCounter = 100
+//gameCounter++;
+++gameCounter
+console.log(gameCounter);
