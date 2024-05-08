2d9ea95f03a5e56cfe536de5d5fd63be8798932c
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-05-09 00:57:09 +0530

    Update 04_comparision.js

--- .devcontainer/01_basics/04_comparision.js
+++ .devcontainer/01_basics/04_comparision.js
@@ -8,14 +8,21 @@
 console.log("2" > 1);
 console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//=== always comperision same data type value
+
+console.log(null > 0); (result "false")
+console.log(null == 0); (result "false")
+console.log(null >= 0); (result "true")
+
+//=== The reasone of giving the result as false, false, true is that
+//=== The reasone is that an equality check(==) and comperision(> < >= <=) work diferently.
+   // Comperision convert null to a number, treating it as 0.
+
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-//=== (it gives strict cheak it means it also cheak data type of given value)
+//=== ( "===" it gives strict cheak it means it also cheak data type of given value)
 
 console.log("2" === 2);
