53c4f84b0da2c2de4223686bd0ac27d0b9a96685
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-05-09 20:36:08 +0530

    Update datatype-summary.js

--- .devcontainer/01_basics/datatype-summary.js
+++ .devcontainer/01_basics/datatype-summary.js
@@ -1,6 +1,7 @@
 //data type
 
-//primitive
+//primitive (In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties.) 
+//There are 7 primitive data types:
 
 //7 types: string, Number, Boolean, null , undifined, symbol, BigInt
 
