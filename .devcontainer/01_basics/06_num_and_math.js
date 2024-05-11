32f1e530f54edc024df6d9111567c03acdda691b
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-05-11 15:19:03 +0000

    push

--- .devcontainer/01_basics/06_num_and_math.js
+++ .devcontainer/01_basics/06_num_and_math.js
@@ -11,4 +11,34 @@ console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000
 console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString("en-US", {style:"currency", currency:"USD"}));
+
+console.log(hundreds.valueOf(1000000));
+
+
+
+//------------------------------------------------------------
+
+
+//---------------------Maths++++++++++++++++++++++++++++++++++
+
+//console.log(Maths);
+console.log(Math.abs(-4));
+console.log(Math.round(3.52));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(6.9));
+console.log(Math.min(4,2,5,6));
+console.log(Math.max(4,5,7,9));
+console.log(Math.PI);
+console.log(Math.sqrt(2));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+
+const min = 10
+const max =20
+console.log(Math.floor(Math.random()*(max-min +1)) + min);
+
 
