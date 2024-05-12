0cd61f551d08baa89af0887ccb847689c28a3e3a
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-05-12 12:40:50 +0000

    push

---
+++ .devcontainer/01_basics/07_date.js
@@ -0,0 +1,31 @@
+//Date
+
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getFullYear());
+
+let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toString());
+
+//let myCreatedDate = new Date(2023,0,23,5,3)
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getDay());
+console.log(newDate.getMonth());
+
+
