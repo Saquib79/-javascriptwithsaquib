33d58a18787c7103789277f5352cd41d56bcbc5e
Saquib79 <161459181+Saquib79@users.noreply.github.com>
2024-05-13 08:10:31 +0000

    arrays

--- .devcontainer/01_basics/07_date.js
+++ .devcontainer/01_basics/07_date.js
@@ -2,10 +2,10 @@
 
 let myDate = new Date()
 //console.log(myDate);
-//console.log(myDate.toString());
-//console.log(myDate.toDateString());
-//console.log(myDate.toLocaleDateString());
-//console.log(myDate.toISOString());
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
 console.log(myDate.toLocaleTimeString());
 console.log(myDate.toTimeString());
 console.log(myDate.toUTCString());
---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+
+//console.log(myArr[1]);
+
+//const myHeros = ["shaktiman", "ironman"]
+//console.log(myHeros[1]);
+
+//Arrys Method
+
+///myArr.push(6)
+//console.log(myArr);
+
+//myArr.pop()
+//console.log(myArr);
+
+//myArr.unshift(9)
+//console.log(myArr);
+
+//myArr.shift()
+//console.log(myArr);
+
+
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
